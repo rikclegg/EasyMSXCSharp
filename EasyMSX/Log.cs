94de73062a667e33ae2264b34c66ba29c873b8e9
rclegg2 <rclegg2@bloomberg.net>
2018-05-01 16:40:17 +0100

    Added logPrefix to Log class

--- EasyMSX/Log.cs
+++ EasyMSX/Log.cs
@@ -11,16 +11,16 @@ namespace com.bloomberg.emsx.samples {
 	    }
 	
 	    public static LogLevels logLevel=LogLevels.NONE;
-	
-	    public static void LogMessage(LogLevels level, string str) {
+        public static String logPrefix = "";
+
+        public static void LogMessage(LogLevels level, string str) {
 	
 		    if(logLevel==LogLevels.NONE) return;
 		    if(level==LogLevels.DETAILED && logLevel==LogLevels.BASIC) return;
-		
-            DateTime date = DateTime.Now;
-            System.Console.WriteLine(date.ToString("yyyy/MM/dd HH:mm:ss.fff") + "\t" + str);
-		
-	    }
+
+            System.Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfffzzz") + logPrefix + ": \t" + str);
+
+        }
 
     }
 }
